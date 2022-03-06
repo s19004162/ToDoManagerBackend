9051c36f448c435d1c11bfaafa50cc7732d640f5
TSUYOSHI KIMOTO <tk_automate@outlook.com>
2022-03-06 22:49:21 +0900

    Fixing crossOrigin and JsonIgnoreProperties
    Fixing crossOrigin and JsonIgnoreProperties

--- src/main/java/com/tk/todoapp/todoapp/dto/ToDo.java
+++ src/main/java/com/tk/todoapp/todoapp/dto/ToDo.java
@@ -1,7 +1,9 @@
 package com.tk.todoapp.todoapp.dto;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 
 @Data
+@JsonIgnoreProperties({"hibernateLazyInitializer"})
 public class ToDo {
 
     private Long id;
